1d74691a420915352e8139cba18f01a8c8687582
George Dobreff <gm@witcher.blog>
2025-10-10 17:06:38 +0100

    Initial commit with basic bot code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Discord AI Bot
+
+**This is a work in progress**
+
+The end goal is to have a **completely free** AI agent with chat, web search, voice and image generation capabilities all inside a discord bot that you can add to your server.
+
+I'm doing this so I can practice JS and LLM implementation while making something cool.
+
+I might sneak in some Python too if necessary.
---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+// Basic ping to ensure the bot is working. Used for testing.
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong and latency information!'),
+    async execute(interaction) {
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        const pingTime = sent.createdTimestamp - interaction.createdTimestamp;
+        
+        await interaction.editReply(`Pong! 🏓\nBot Latency: ${pingTime}ms\nAPI Latency: ${Math.round(interaction.client.ws.ping)}ms`);
+    },
+};
